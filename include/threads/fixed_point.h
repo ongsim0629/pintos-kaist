c1fb175da5d74dc69abeb5fd3ae9e8cd221e91ef
Ubuntu <ubuntu@ip-172-31-5-7.ap-northeast-2.compute.internal>
2024-09-30 06:57:52 +0000

    fixed_point calculators

--- include/threads/fixed_point.h
+++ include/threads/fixed_point.h
@@ -13,7 +13,54 @@ int add_mixed(int x, int n);
 int sub_fp(int x, int y);
 int sub_mixed(int x, int n);
 int mult_fp(int x, int y);
-int mult_mixed(int x, int y);
+int mult_mixed(int x, int n);
 int div_fp(int x, int y);
 int div_mixed(int x, int n);
-/* 함수 본체 작성 */
+
+int int_to_fp(int n)  {
+    return n * F;
+}
+
+int fp_to_int_round(int x) {
+    if (x >= 0) {
+        return (x + F / 2) / F;
+    } else {
+        return (x - F / 2) / F;
+    }
+}
+
+int fp_to_int(int x) {
+    return x / F;
+}
+
+int add_fp(int x, int y) {
+    return x + y;
+}
+
+int add_mixed(int x, int n) {
+    return x + n * F;
+}
+
+int sub_fp(int x, int y) {
+    return x - y;
+}
+
+int sub_mixed(int x, int n) {
+    return x - n * F;
+}
+
+int mult_fp(int x, int y) {
+    return ((int64_t)x * y) / F;
+}
+
+int mult_mixed(int x, int n) {
+    return x * n;
+}
+
+int div_fp(int x, int y) {
+    return ((int64_t)x * F) / y;
+}
+
+int div_mixed(int x, int n) {
+    return x / n;
+}
