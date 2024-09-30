8276ffa748f5b30ba508f244b4e458abaea33b25
Ubuntu <ubuntu@ip-172-31-5-7.ap-northeast-2.compute.internal>
2024-09-30 04:38:49 +0000

    set base of mlfq 2

--- threads/thread.c
+++ threads/thread.c
@@ -11,6 +11,7 @@
 #include "threads/synch.h"
 #include "threads/vaddr.h"
 #include "intrinsic.h"
+#include "threads/fixed_point.h"
 #ifdef USERPROG
 #include "userprog/process.h"
 #endif
@@ -145,6 +146,7 @@ thread_start (void) {
 	struct semaphore idle_started;
 	sema_init (&idle_started, 0);
 	thread_create ("idle", PRI_MIN, idle, &idle_started);
+	load_avg = LOAD_AVG_DEFAULT;
 
 	/* Start preemptive thread scheduling. */
 	intr_enable ();
@@ -373,6 +375,8 @@ thread_get_nice (void) {
 }
 
 /* Returns 100 times the system load average. */
+// 현재 sysyem load average의 100배 반환
+// timer_ticks() % TIMER_FREQ == 0
 int
 thread_get_load_avg (void) {
 	/* TODO: Your implementation goes here */
@@ -448,6 +452,9 @@ init_thread (struct thread *t, const char *name, int priority) {
 	t->tf.rsp = (uint64_t) t + PGSIZE - sizeof (void *);
 	t->priority = priority;
 	t->magic = THREAD_MAGIC;
+
+	t->nice = NICE_DEFAULT; 
+	t->recent_cpu = RECENT_CPU_DEFAULT;
 }
 
 /* Chooses and returns the next thread to be scheduled.  Should
@@ -666,4 +673,20 @@ void thread_wakeup (int64_t ticks) {
 	}
 
 
+}
+
+void mlfqs_priority (struct thread *t) {
+
+} 
+void mlfqs_recent_cpu (struct thread *t) {
+
+}
+void mlfqs_load_avg (void) {
+
+}
+void mlfqs_increment (void) {
+
+}
+void mlfqs_recalc (void) {
+	
 }
